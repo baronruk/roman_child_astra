1b28adc520e6f0ebf6f476b39799a2e200c5e8d5
baronruk <79830650+baronruk@users.noreply.github.com>
2021-06-07 11:40:24 +0300

    gitingore and bootstrap js

--- functions.php
+++ functions.php
@@ -16,12 +16,15 @@ function load_custom_files() {
 	// load child theme css
 	wp_register_style('custom_css', get_stylesheet_directory_url(). '/assets/css/custom.css');
 	wp_enqueue_style('custom_css');
-	// Add bootstrap CSS
+	// Add bootstrap css
     wp_register_style( 'bootstrap-css', get_stylesheet_directory_url(). '/assets/css/bootstrap.css' );
     wp_enqueue_style( 'bootstrap-css' );
 	// load child theme js
 	wp_register_style('custom_js', get_stylesheet_directory_url(). '/assets/js/custom.js');
 	wp_enqueue_style('custom_js');
+	// add bootstrap js
+	wp_register_style('bootstrap-js', get_stylesheet_directory_url(). '/assets/js/bootstrap.bundle.js');
+	wp_enqueue_style('bootstrap-js');
 }
 add_action('wp_enqueue_scripts', 'load_custom_files', 10);
 
