34e2dc03510e3a7c7a7bbb4fe26916a96b0b9e59
baronruk <79830650+baronruk@users.noreply.github.com>
2021-06-07 11:18:47 +0300

    bootstrap

--- functions.php
+++ functions.php
@@ -16,6 +16,9 @@ function load_custom_files() {
 	// load child theme css
 	wp_register_style('custom_css', get_stylesheet_directory_url(). '/assets/css/custom.css');
 	wp_enqueue_style('custom_css');
+	// Add bootstrap CSS
+    wp_register_style( 'bootstrap-css', get_stylesheet_directory_url(). '/assets/css/bootstrap.css' );
+    wp_enqueue_style( 'bootstrap-css' );
 	// load child theme js
 	wp_register_style('custom_js', get_stylesheet_directory_url(). '/assets/js/custom.js');
 	wp_enqueue_style('custom_js');
--- template_1.php
+++ template_1.php
@@ -4,5 +4,71 @@
 */
 
 get_header(); ?>
-<?php the_content(); ?>
+
+   <main>
+<!-- Section 1 Start -->
+        <section class="hero">
+            <div class="container">
+                <div class="row h-100 justify-content-center align-items-center">
+                  <div class="col-12 text-center">
+                    <h1>Όνομα Επίθετο</h1>
+                    <p>Web Developer</p>
+                  </div>
+                </div>
+            </div>
+        </section>
+<!-- Section 1 End -->
+<!-- Section 2 Start -->
+        <section class="profile">
+            <div class="container h-100">
+                <div class="row h-100 justify-content-start align-items-center">
+                    <div class="col-12 col-lg-6 text-center">
+                        <div class="image_wrapper">
+                            <img class="image_profile" src="assets/images/placeholder_image_520w.jpg" />
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6">
+                        <div class="container h-100">
+                            <div class="row h-100 justify-content-start align-items-center">
+                                <div class="d-flex h-75 flex-column align-items-start">
+                                    <h2>Όνομα Επίθετο</h2>
+                                    <span>Web Developer</span>
+                                    <p>Text</p>
+                                    <button type="button" class="btn btn-primary mt-auto">Primary</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<!-- Section 2 End -->
+<!-- Section 3 Start -->
+        <section class="services py-5 bg-light">
+            <div class="container h-100">
+                <div class="row h-100 justify-content-around align-items-center row-cols-1 row-cols-sm-2 g-3">
+                    <article class="col-md-4 text-center test">
+                        <h5>Προγραμματισμός</h5>
+                    </article>
+                    <article class="col-md-4 text-center test">
+                        <h5>Σχεδίαση</h5>
+                    </article>
+                    <article class="col-md-4 text-center test">
+                        <h5>Web Design</h5>
+                    </article>
+                    <article class="col-md-4 text-center test">
+                        <h5>Φωτογραφία</h5>
+                    </article>
+                    <article class="col-md-4 text-center test">
+                        <h5>Video</h5>
+                    </article>
+                    <article class="col-md-4 text-center test">
+                        <h5>Εφαρμογές</h5>
+                    </article>
+                </div>
+            </div>           
+        </section>
+<!-- Section 3 End -->
+    </main>
+
 <?php get_footer(); ?>
